bd421ff8ebd3a85ef97b6c7647cceebefb48df4a
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-02-15 19:51:18 +0200

    step1

--- part2.6/src/App.js
+++ part2.6/src/App.js
@@ -6,19 +6,40 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+const addName = (event) =>{
+    event.preventDefault()
+    const nameObject = {
+        name: newName,
+        id: newName
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+    }
+
+const handleNameChange = (event) =>{
+    console.log(event.target.value);
+    setNewName(event.target.value)
+}   
+
+console.log(newName);
+console.log(persons)
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+        onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      //something
+      <div>{persons.map(persons => 
+        <p key={persons.name}>{persons.name}</p>
+        )}</div>
     </div>
   )
 }
